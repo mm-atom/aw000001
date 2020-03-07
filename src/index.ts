8e89a7eb8a7df91c8cbe4cbee65a09e2b12d8f47
taoqf <tao_qf@163.com>
2020-03-07 14:24:10 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-export default function fun() {
+export interface IHandle {
+	destroy(this: IHandle): void;
+}
+
+export default interface IAiBrowserComponent {
+	readonly data: {
+		readonly [attr: string]: unknown;
+		readonly no: string;
+		readonly node: HTMLElement;
+		readonly params: { [key: string]: unknown };
+		readonly url: string;
+	};
+	parse_event(node: HTMLElement | DocumentFragment): IHandle[];
+	fire<T>(action: string, ...args: unknown[]): Promise<T>;
+	emit<T>(event: string, ...args: unknown[]): Promise<T>;
+	// eslint-disable-next-line semi
 }
